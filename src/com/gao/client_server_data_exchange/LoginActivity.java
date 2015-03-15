a8f62f715e766aa00ad5ee04719cfcd0cf783c56
GaoMatrix <gcquan08@gmail.com>
2015-03-16 06:26:57 +0800

    Initial project

---
+++ src/com/gao/client_server_data_exchange/LoginActivity.java
@@ -0,0 +1,16 @@
+
+package com.gao.client_server_data_exchange;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity);
+    }
+}
