9d3328ca70c88352f56f0a67cb6b0bef962ad398
GaoMatrix <gcquan08@gmail.com>
2015-03-17 06:59:22 +0800

    “注册”DEMO实战-Android封装Json数据，发送请求

--- src/com/gao/client_server_data_exchange/service/UserServiceImpl.java
+++ src/com/gao/client_server_data_exchange/service/UserServiceImpl.java
@@ -1,6 +1,8 @@
 
 package com.gao.client_server_data_exchange.service;
 
+import android.graphics.Point;
+
 import com.gao.client_server_data_exchange.LoginActivity;
 
 import org.apache.http.HttpResponse;
@@ -13,6 +15,8 @@ import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.protocol.HTTP;
 import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -56,6 +60,47 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public void userRegister(String loginName, List<String> interesting) throws Exception {
-        Thread.sleep(2000);
+        HttpClient httpClient = new DefaultHttpClient();
+        String uri = "http://192.168.1.2:8080/Client_Server_Data_Exchange/register.do";
+        HttpPost httpPost = new HttpPost(uri);
+        //JAON数据的封装
+        // Data = {"LoginName":"Tom", "Interesting":["game","music","sports"]}
+        JSONObject object = new JSONObject();
+        object.put("LoginName", loginName);
+        JSONArray array = new JSONArray();
+        if (null != interesting) {
+            for (String string : interesting) {
+                array.put(string);
+            }
+          }
+        object.put("Interesting", array);
+        NameValuePair parameter = new BasicNameValuePair("Data ", object.toString());
+        List<NameValuePair> params = new ArrayList<NameValuePair>();
+        params.add(parameter);
+        
+        httpPost.setEntity(new UrlEncodedFormEntity(params, HTTP.UTF_8));
+        HttpResponse httpResponse = httpClient.execute(httpPost);
+        
+        int statusCode = httpResponse.getStatusLine().getStatusCode();
+        if (statusCode != HttpStatus.SC_OK) {//200
+            throw new ServiceRulesException(LoginActivity.MSG_SERVER_ERROR);
+        }
+        String result = EntityUtils.toString(httpResponse.getEntity(), HTTP.UTF_8);
+        if (result.equals("success")) {
+            
+        } else {
+            throw new ServiceRulesException(LoginActivity.MSG_LOGIN_FAILED);
+        }
+     }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
     }
 }
